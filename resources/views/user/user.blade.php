a6daf4b1a6a79d838b86407d8d4e65c73b8f38c3
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-11-12 11:45:38 -0300

    Criado Route, Controller e View users

---
+++ resources/views/user/user.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Listar usuários - App-Sistema')
+
+@section('content')
+    <x-header-component/>
+
+    <!-- Conteúdo principal -->
+    <div class="container mt-4 text-center">
+        <h1>Página Listar</h1>
+        <!-- O conteúdo das páginas será inserido aqui -->
+    </div>
+
+    <x-footer-component/>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'index']);
+
+Route::get('/user/index', [UsersController::class, 'index']);
