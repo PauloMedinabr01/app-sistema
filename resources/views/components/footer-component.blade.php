cbf14df788edba37d87ff7661af2b8526db6f48a
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-11-12 12:44:35 -0300

    Ajuste no footer

--- resources/views/components/footer-component.blade.php
+++ resources/views/components/footer-component.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-light text-center py-3">
+<footer class="bg-dark text-light text-center py-3 mt-4">
     &copy; 2023 App-Sistema. Todos os direitos reservados.
 </footer>
