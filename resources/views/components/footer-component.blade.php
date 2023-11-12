abf544e0c2d000f8dac39bda655efee0ead4233a
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-11-12 10:50:37 -0300

    Criados Componentes e layout padrão e página index

---
+++ resources/views/components/footer-component.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light text-center py-3">
+    &copy; 2023 App-Sistema. Todos os direitos reservados.
+</footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Página inicial</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.app')
 
-<h1>Página inicial</h1>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous">
-</script>
+@section('content')
+    <x-header-component/>
 
-</body>
-</html>
+    <!-- Conteúdo principal -->
+    <div class="container mt-4 text-center">
+        <h1>Página inicial</h1>
+        <!-- O conteúdo das páginas será inserido aqui -->
+    </div>
+
+    <x-footer-component/>
+@endsection
